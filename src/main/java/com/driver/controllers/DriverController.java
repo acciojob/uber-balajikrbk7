1b72556c18bc7f7017ba670db6cefd161863eeec
Balaji K R <86788000+balajikrbk7@users.noreply.github.com>
2023-02-25 21:44:09 +0530

    Update DriverController.java

--- src/main/java/com/driver/controllers/DriverController.java
+++ src/main/java/com/driver/controllers/DriverController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(value = "/driver")
 public class DriverController {
+
 	@Autowired
 	DriverService driverService;
 	@PostMapping(value = "/register")
